fce004cdcda15414a9ee14dda709c2bde11f0897
Doddddd <1524010598@qq.com>
2025-01-17 21:47:16 +0800

    fix compile warn

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -570,8 +570,9 @@ int main(int ac, char **av)
 				defconfig_file);
 			exit(1);
 		}
+
 		name = getenv("KCONFIG_VARIANT");
-		printf("KCONFIG_VARIANT(%s)\n", name);
+		printf("info: KCONFIG_VARIANT(%s)\n", name);
 		if (name) {
 			if (conf_read_simple(name, S_DEF_USER, false)) {
 				fprintf(stderr,
@@ -584,7 +585,7 @@ int main(int ac, char **av)
 		}
 
 		name = getenv("KCONFIG_DEBUG");
-		printf("KCONFIG_DEBUG(%s)\n", name);
+		printf("info: KCONFIG_DEBUG(%s)\n", name);
 		if (name) {
 			if (conf_read_simple(name, S_DEF_USER, false)) {
 				fprintf(stderr,
