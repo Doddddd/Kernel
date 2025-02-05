c732bc9c8248848490ab6ba95d840970e438e7e7
Doddddd <1524010598@qq.com>
2025-02-05 16:34:51 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+TIME=$(date +%s)
+
+if [[ -d "$HOME/tool/Clang/r383902b1" ]]; then
+    export PATH=$HOME/tool/Clang/r383902b1/bin:$PATH
+else
+    echo "缺少 Clang r383902b1"
+    exit 1
+fi
+
+ARGS=(LOCALVERSION=lto LLVM=1 LLVM_IAS=1 DTC_EXT="$(pwd)/tools/dtc" CONFIG_BUILD_ARM64_DT_OVERLAY=y ARCH=arm64 CROSS_COMPILE="$HOME/tool/GCC/bin/aarch64-linux-android-" REAL_CC="ccache clang" CLANG_TRIPLE=aarch64-linux-gnu-)
+
+if [[ "$*" =~ "clean" ]]; then
+    make "${ARGS[@]}" mrproper
+    rm out -rf
+fi
+
+make -j"$(nproc)" -C "$(pwd)" O="$(pwd)/out" "${ARGS[@]}" vendor/o1q_chn_hkx_defconfig
+make -j"$(nproc)" -C "$(pwd)" O="$(pwd)/out" "${ARGS[@]}"
+
+OUTPUT=../output/$(basename "$(pwd)")
+OUTPUT_NAME=$(basename "$(pwd)")_$(date +%F_%H-%M-%S)
+mkdir "$OUTPUT/old" -p
+
+find "$OUTPUT" -maxdepth 1 -type f \( -name "*.zip" -o -name "*.tar" \) -exec mv {} "$OUTPUT/old/" \;
+
+cp ./out/arch/arm64/boot/Image ../AnyKernel3/zImage
+cd ../AnyKernel3
+zip -r "$OUTPUT/$OUTPUT_NAME.zip" ./*
+
+if [[ -f "$OUTPUT/origin_boot.img" ]]; then
+    magiskboot unpack "$OUTPUT/origin_boot.img"
+    mv zImage kernel
+    magiskboot repack -n "$OUTPUT/origin_boot.img" boot.img
+    tar cf "$OUTPUT/$OUTPUT_NAME.tar" boot.img
+fi
+
+rm zImage kernel boot.img -rf
+
+echo -e "\e[32m   用时: $((($(date +%s) - TIME) / 60))m$((($(date +%s) - TIME) % 60))s\e[0m"
